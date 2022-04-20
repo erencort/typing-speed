e259e1742d98ece741c14fad41d2bcc98c3836eb
eren <erencort1@gmail.com>
2022-04-20 18:38:29 +0300

    commit 3 | design the ui

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -6,6 +6,9 @@ import { setWords } from "../redux/wordSlice";
 function Words() {
   //   const [words, setWords] = useState([]);
   const words = useSelector((state) => state.typing.words);
+  const activeWord = useSelector((state) => state.typing.activeWord);
+  const failedWords = useSelector((state) => state.typing.failedWords);
+  const successfulWords = useSelector((state) => state.typing.successfulWords);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,7 +18,12 @@ function Words() {
   return (
     <div className="words-section">
       {words.map((item, i) => (
-        <p key={i} className="word">
+        <p
+          key={i}
+          className={`word ${activeWord == item ? "background" : ""} ${
+            failedWords.includes(item) ? "failed" : ""
+          } ${successfulWords.includes(item) ? "successful" : ""}`}
+        >
           {item}
         </p>
       ))}
--- src/redux/wordSlice.js
+++ src/redux/wordSlice.js
@@ -7,17 +7,19 @@ export const wordSlice = createSlice({
     words: [],
     countdown: 60,
     userInput: "",
-    word: "",
+    activeWord: "",
     turn: 0,
     success: 0,
     failed: 0,
     status: "firstMounted",
     playedTimes: 0,
+    failedWords: [],
+    successfulWords: [],
   },
   reducers: {
     setWords: (state, action) => {
-      state.words = randomWords(200);
-      state.word = state.words[0];
+      state.words = randomWords({ exactly: 200 });
+      state.activeWord = state.words[0];
     },
     setCountdown: (state, action) => {
       state.countdown = state.countdown - 1;
@@ -29,25 +31,29 @@ export const wordSlice = createSlice({
       state.playedTimes = state.playedTimes + 1;
       if (state.playedTimes >= 2) {
         state.words = randomWords(200);
-        state.word = state.words[0];
+        state.activeWord = state.words[0];
       }
       state.countdown = 60;
       state.turn = 0;
       state.success = 0;
       state.failed = 0;
+      state.failedWords = [];
+      state.successfulWords = [];
       state.status = "running";
     },
     setUserInput: (state, action) => {
       state.userInput = action.payload;
     },
     compareWords: (state, action) => {
-      if (state.userInput === state.word) {
+      if (state.userInput === state.activeWord) {
         state.success = state.success + 1;
+        state.successfulWords = [...state.successfulWords, state.activeWord];
       } else {
         state.failed = state.failed + 1;
+        state.failedWords = [...state.failedWords, state.activeWord];
       }
       state.turn = state.turn + 1;
-      state.word = state.words[state.turn];
+      state.activeWord = state.words[state.turn];
     },
   },
 });
